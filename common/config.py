1c60a03d708b415b93fb8f1a3582765596790b75
Satwik <veluvalisaisatwik7@gmail.com>
2026-05-28 15:16:54 +0530

    fixed db config

--- common/config.py
+++ common/config.py
@@ -1,15 +1,13 @@
 import psycopg2
 
-db_host = "dpg-d8c0ctrbc2fs738n8nog-a"
+db_host = "dpg-d8c0ctrbc2fs738n8nog-a.oregon-postgres.render.com"
 db_user = "datatype_user"
 db_password = "atbM4c8RzAA4wlEGg9ueBzi2zHEEeUc2"
 db_port = "5432"
 db_name = "upd_7g4e"
 
 def db_connection():
-
     try:
-
         connection = psycopg2.connect(
             host=db_host,
             user=db_user,
